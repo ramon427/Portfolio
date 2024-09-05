f389633aa3470c0f70a69e4a0f592c8760dcfb7c
turtlesoupVEVO <ramonverheuvel@gmail.com>
2024-09-05 16:26:34 +0200

    Test

---
+++ java/src/main/java/me/github/turtlesoupvevo/java/PortfolioMain.java
@@ -0,0 +1,13 @@
+package me.github.turtlesoupvevo.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioMain.class, args);
+	}
+
+}
